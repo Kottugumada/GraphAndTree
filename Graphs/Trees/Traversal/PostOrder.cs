ebdbc83a526d96c9841f3863b5c57ff54295d30c
Karthik <Kottugumada>
2019-02-12 22:19:34 -0800

    Traversal Added

---
+++ Graphs/Trees/Traversal/PostOrder.cs
@@ -0,0 +1,28 @@
+ // Definition for a binary tree node.
+ public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int x) { val = x; }
+ }
+public class Solution {
+    public IList<int> InOrderTraversal(TreeNode root) {
+        IList<int> res = new List<int>();
+        if(root == null) return res;
+
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        TreeNode curr = root;
+        while(curr != null || st.Any()){
+            if(curr != null){
+            st.Push(curr);
+            res.Insert(0,curr.val);
+            curr = curr.right;
+            }
+            else{
+                curr = st.Pop();
+                curr = curr.left;
+            }
+        }
+        return res;
+    }
+}
