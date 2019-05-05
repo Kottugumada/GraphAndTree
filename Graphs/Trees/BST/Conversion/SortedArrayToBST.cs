52087eb71b40cf2c96988e7b504d3995d0c4e09f
Karthik <karthikkottugumada@outlook.com>
2019-05-05 11:11:39 -0700

    Bug fix

--- Graphs/Trees/BST/Conversion/SortedArrayToBST.cs
+++ Graphs/Trees/BST/Conversion/SortedArrayToBST.cs
@@ -8,14 +8,15 @@ public class TreeNode{
 }
 public class Solution{
     public TreeNode SortedArrayToBST(int[] nums){
+        if(nums == null) return null;
         return Helper(nums, 0, nums.Length-1);
     }
     private TreeNode Helper(int[] nums, int l, int r){
         if(l>r) return null;
         int mid = (l + r)/2;
-        TreeNode root = new TreeNode(mid);
-        root.left = Helper(nums, l, mid -1);
-        root.right = Helper(nums, mid, r);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = Helper(nums, l, mid - 1);
+        root.right = Helper(nums, mid + 1, r);
         return root;
     }
 }
