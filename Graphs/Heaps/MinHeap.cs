333b387ef530fcb516150b427354b10d5b89ab17
Karthik <karthikkottugumada@outlook.com>
2019-05-11 12:42:25 -0700

    MinHeap and MaxHeap Code Cleanup

--- Graphs/Heaps/MinHeap.cs
+++ Graphs/Heaps/MinHeap.cs
@@ -1,94 +1,111 @@
 ﻿using System;
 
-namespace DataStructures.Heap
+namespace Heaps
 {
     public class MinHeap
     {
-        public static int capacity = 10;
+        private static int capacity = 10;
         private int size = 0;
 
-        public int[] items = new int[capacity];
-        private int GetLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
-        private int GetRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
-        private int GetParentindex(int childIndex) { return (childIndex - 1) / 2; }
+        int[] items = new int[capacity];
 
-        private bool HasLeftChild(int index) { return GetLeftChildIndex(index) < size; }
-        private bool HasRightChild(int index) { return GetRightChildIndex(index) < size; }
-        private bool HasParent(int index) { return GetParentindex(index) >= 0; }
+        private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
+        private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
+        private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; }
 
-        private int GetLeftChild(int index) { return items[GetLeftChildIndex(index)]; }
-        private int GetRightChild(int index) { return items[GetRightChildIndex(index)]; }
-        private int GetParent(int index) { return items[GetParentindex(index)]; }
+        private bool hasLeftChild(int index) { return getLeftChildIndex(index) < size; }
+        private bool hasRightChild(int index) { return getRightChildIndex(index) < this.size; }
+        private bool hasParent(int index) { return getParentIndex(index) >= 0; }
 
-        private void Swap(int indexOne, int indexTwo)
+        private int leftChild(int index) { return this.items[getLeftChildIndex(index)]; }
+        private int rightChild(int index) { return this.items[getRightChildIndex(index)]; }
+        private int parent(int index) { return this.items[this.getParentIndex(index)]; }
+
+        private void swap(int indexOne, int indexTwo)
         {
-            int temp = items[indexOne];
-            items[indexOne] = items[indexTwo];
-            items[indexTwo] = temp;
+            int temp = this.items[indexOne];
+            this.items[indexOne] = this.items[indexTwo];
+            this.items[indexTwo] = temp;
         }
 
-        private void EnsureExtraCapacity()
+        private void ensureExtraCapacity()
         {
-            if (size == capacity)
+            if (this.size == capacity)
             {
-                int[] temp = new int[capacity*2];
-                // Array.Copy(items, temp, capacity);
-                temp = items;
-                capacity = capacity * 2;
+                Array.Resize(ref this.items, capacity * 2);
+                capacity *= 2;
             }
         }
-        public int Peek()
+
+        public int peek()
         {
-            if (size == 0) throw new Exception();
-            return items[0];
+            if (this.size == 0) throw new NotSupportedException();
+            return this.items[0];
         }
-        public int Poll()
+
+        public int remove()
         {
-            if (size == 0) throw new Exception();
-            int item = items[0];
-            items[0] = items[size - 1];
-            size--;
-            HeapifyDown();
+            if (this.size == 0) throw new NotSupportedException();
+            int item = this.items[0];
+            items[0] = items[this.size - 1];
+            items[this.size - 1] = 0;
+            this.size--;
+            heapifyDown();
             return item;
         }
+
         public void Add(int item)
         {
-            EnsureExtraCapacity();
-            size++;
-            items[size] = item;
-            HeapifyUp();
+            this.ensureExtraCapacity();
+            this.items[size] = item;
+            this.size++;
+            heapifyUp();
         }
 
-        private void HeapifyUp()
+        private void heapifyUp()
         {
-            int index = size - 1;
-            while (HasParent(index) && GetParent(index)> items[index])
+            int index = this.size - 1;
+            while (hasParent(index) && parent(index) > this.items[index])
             {
-                Swap(GetParentindex(index),index);
-                index = GetParentindex(index);
+                swap(index, getParentIndex(index));
+                index = getParentIndex(index);
             }
         }
 
-        private void HeapifyDown()
+        private void heapifyDown()
         {
             int index = 0;
-            while (HasLeftChild(index))
+            while (hasLeftChild(index))
             {
-                int smallerChildIndex = GetLeftChildIndex(index);
-                if (HasRightChild(index) && GetRightChildIndex(index)< GetLeftChildIndex(index))
+                int smallerChildIndex = getLeftChildIndex(index);
+                if (hasRightChild(index) && rightChild(index) < leftChild(index))
                 {
-                    smallerChildIndex = GetRightChildIndex(index);
+                    smallerChildIndex = getRightChildIndex(index);
                 }
-                if (items[index] <items[smallerChildIndex])
+
+                if (this.items[index] < this.items[smallerChildIndex])
                 {
                     break;
                 }
                 else
                 {
-                    Swap(index, smallerChildIndex);
+                    swap(index, smallerChildIndex);
                 }
                 index = smallerChildIndex;
             }
         }
     }
 }
+
+/*
+Calling Code:
+    MinHeap mh = new MinHeap();
+    mh.Add(10);
+    mh.Add(5);
+    mh.Add(2);
+    mh.Add(1);
+    mh.Add(50);
+    int peek = mh.peek();
+    mh.remove();
+    int newPeek = mh.peek();
+*/
