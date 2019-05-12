1440122d3c590ec2324951aa139cc8af2914ed96
Karthik <karthikkottugumada@outlook.com>
2019-05-12 12:06:48 -0700

    LaxHeap and Level Order traversal

--- Graphs/Heaps/MaxHeap.cs
+++ Graphs/Heaps/MaxHeap.cs
@@ -2,69 +2,69 @@
 
 namespace Heaps
 {
-    class MaxHeap
+    public class MaxHeap
     {
         private static int capacity = 10;
-        private int size = 0;
-        int[] items = new int[capacity];
+        private static int size = 0;
+        static int[] items = new int[capacity];
 
-        private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
-        private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
-        private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; }
+        private static int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
+        private static int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
+        private static int getParentIndex(int childIndex) { return (childIndex - 1) / 2; }
 
-        private int getLeftChild(int parentIndex) { return this.items[getLeftChildIndex(parentIndex)]; }
-        private int getRightChild(int parentIndex) { return this.items[getRightChildIndex(parentIndex)]; }
-        private int getParent(int childIndex) { return this.items[getParentIndex(childIndex)]; }
+        private static int getLeftChild(int parentIndex) { return items[getLeftChildIndex(parentIndex)]; }
+        private static int getRightChild(int parentIndex) { return items[getRightChildIndex(parentIndex)]; }
+        private static int getParent(int childIndex) { return items[getParentIndex(childIndex)]; }
 
-        private bool hasLeftChild(int parentIndex) { return getLeftChildIndex(parentIndex) < size; }
-        private bool hasRightChild(int parentIndex) { return getRightChildIndex(parentIndex) < size; }
-        private bool hasParent(int childIndex) { return getLeftChildIndex(childIndex) > 0; }
+        private static bool hasLeftChild(int parentIndex) { return getLeftChildIndex(parentIndex) < size; }
+        private static bool hasRightChild(int parentIndex) { return getRightChildIndex(parentIndex) < size; }
+        private static bool hasParent(int childIndex) { return getLeftChildIndex(childIndex) > 0; }
 
-        private void swap(int indexOne, int indexTwo)
+        private static void swap(int indexOne, int indexTwo)
         {
-            int temp = this.items[indexOne];
-            this.items[indexOne] = this.items[indexTwo];
-            this.items[indexTwo] = temp;
+            int temp = items[indexOne];
+            items[indexOne] = items[indexTwo];
+            items[indexTwo] = temp;
         }
 
-        private void hasEnoughCapacity()
+        private static void hasEnoughCapacity()
         {
-            if (this.size == capacity)
+            if (size == capacity)
             {
-                Array.Resize(ref this.items, capacity * 2);
+                Array.Resize(ref items, capacity * 2);
                 capacity *= 2;
             }
         }
 
-        public void Add(int item)
+        public static void Add(int item)
         {
-            this.hasEnoughCapacity();
-            this.items[size] = item;
-            this.size++;
+            hasEnoughCapacity();
+            items[size] = item;
+            size++;
             heapifyUp();
         }
 
-        public int Remove()
+        public static int Remove()
         {
-            int item = this.items[0];
-            this.items[0] = this.items[size - 1];
-            this.items[this.size - 1] = 0;
+            int item = items[0];
+            items[0] = items[size - 1];
+            items[size - 1] = 0;
             size--;
             heapifyDown();
             return item;
         }
 
-        private void heapifyUp()
+        private static void heapifyUp()
         {
-            int index = this.size - 1;
-            while (hasParent(index) && this.items[index] > getParent(index))
+            int index = size - 1;
+            while (hasParent(index) && items[index] > getParent(index))
             {
                 swap(index, getParentIndex(index));
                 index = getParentIndex(index);
             }
         }
 
-        private void heapifyDown()
+        private static void heapifyDown()
         {
             int index = 0;
             while (hasLeftChild(index))
@@ -75,7 +75,7 @@ namespace Heaps
                     bigChildIndex = getRightChildIndex(index);
                 }
 
-                if (this.items[bigChildIndex] < this.items[index])
+                if (items[bigChildIndex] < items[index])
                 {
                     break;
                 }
---
+++ Graphs/Trees/BST/Traversal/LevelOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Trees.BST.Traversal
+{
+   public class LevelOrder
+    {
+        public IList<IList<int>> LevelOrderTraversal(TreeNode root)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+            if (root == null) return res;
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count >0)
+            {
+                int size = q.Count;
+                List<int> lvl = new List<int>();
+                while (size>0)
+                {
+                    TreeNode node = q.Dequeue();
+                    lvl.Add(node.val);
+                    if (node.left != null) q.Enqueue(node.left);
+                    if (node.right != null) q.Enqueue(node.right);
+                    size--;
+                }
+                res.Add(lvl);
+            }
+            return res;
+        }
+    }
+}
