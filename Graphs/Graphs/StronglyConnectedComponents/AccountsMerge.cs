2137951951a8539f7cbf3466f2a982f877c93b02
Karthik <karthikkottugumada@outlook.com>
2020-05-03 11:16:26 -0700

    few more

---
+++ Graphs/Graphs/StronglyConnectedComponents/AccountsMerge.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs.Heaps.Trees.Graphs.StronglyConnectedComponents
+{
+    public class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/accounts-merge/
+        /// </summary>
+        /// <param name="accounts"></param>
+        /// <returns></returns>
+        public IList<IList<string>> AccountsMerge(IList<IList<string>> accounts)
+        {
+            // email node and neighbor nodes mapping
+            Dictionary<string, HashSet<string>> graph = new Dictionary<string, HashSet<string>>();
+            // email and user name mapping
+            Dictionary<string, string> emailNameMap = new Dictionary<string, string>();
+
+            // construct graph
+            foreach (var account in accounts)
+            {
+                // first value is user name
+                string userName = account[0];
+                for (int i = 1; i < account.Count; i++)
+                {
+                    // other following values on the list are email addresses
+                    if (!graph.ContainsKey(account[i]))
+                    {
+                        graph[account[i]] = new HashSet<string>();
+                    }
+
+                    // if the email id exists add it to the hashset corresponding to the email id
+                    emailNameMap[account[i]] = userName; // assign username to the email
+
+                    if (i == 1)
+                    {
+                        continue;
+                    }
+
+                    // add adjacent email ids as edges
+                    graph[account[i]].Add(account[i - 1]);
+                    graph[account[i - 1]].Add(account[i]);
+                }
+            }
+
+            HashSet<string> visited = new HashSet<string>();
+            IList<IList<string>> res = new List<IList<string>>();
+
+            // DFS
+            foreach (var email in emailNameMap.Keys)
+            {
+                List<string> listOfEmailAddresses = new List<string>();
+                if (visited.Add(email))
+                {
+                    DFS(graph, email, visited, listOfEmailAddresses);
+
+                    listOfEmailAddresses.Add(emailNameMap[email]);
+                    listOfEmailAddresses.Sort(StringComparer.Ordinal); // case insensitive compare
+                    res.Add(listOfEmailAddresses);
+                }
+            }
+            return res;
+        }
+
+        private void DFS(Dictionary<string, HashSet<string>> graph, string email, HashSet<string> visited, List<string> listOfEmailAddresses)
+        {
+            listOfEmailAddresses.Add(email);
+            foreach (var item in graph[email])
+            {
+                if (visited.Add(item))
+                {
+                    DFS(graph, item, visited, listOfEmailAddresses);
+                }
+            }
+        }
+    }
+}
+
---
+++ Graphs/Graphs/Topological Sort/FindTownJudge.cs
@@ -0,0 +1,29 @@
+﻿namespace Graphs.Heaps.Trees.Graphs.Topological_Sort
+{
+    public class FindTownJudge
+    {
+        public int FindJudge(int N, int[][] trust)
+        {
+            int n = trust.Length;
+            if (trust == null || trust.Length == 0)
+            {
+                return N;
+            }
+            // a town judge would have 0 incoming edges
+            int[] inDegree = new int[N+1];
+            foreach (var item in trust)
+            {
+                inDegree[item[0]]++; // if 1 knows 3, increment the number of people who know 3
+                inDegree[item[1]]--; // similarly decrement 1
+            }
+            for (int i = 0; i < inDegree.Length; i++)
+            {
+                if (inDegree[i] == N-1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
